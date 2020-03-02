4c34ba6d394faf9d342118a0c35269211dc19bf1
Eric Dharmasiri <eric.dharmasiri@gmail.com>
2020-03-02 22:49:30 +1100

    Multiple Checkpoints Working Properly
    We need to set conditions for when to respawn, or what to do when the player dies

---
+++ Audio_First_Game_Jam/Assets/Scipts/Level/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private GameMaster gameMaster;
+    private LevelManager levelManager;
+    public GameObject playerObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager = this;
+        gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Condition to respawn the player
+        if (playerObj.transform.position.y < 0)
+            Respawn();
+    }
+
+    public void Respawn()
+    {
+        playerObj.transform.position = gameMaster.lastCheckpointPosition;
+    }
+}
