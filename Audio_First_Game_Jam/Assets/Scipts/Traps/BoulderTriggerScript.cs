c545717ba00d852f79ca051ad4c34c093674bbdd
Kyle Hammer <kylethomashammer@gmail.com>
2020-03-03 09:30:17 +1100

    Merged traps

---
+++ Audio_First_Game_Jam/Assets/Scipts/Traps/BoulderTriggerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderTriggerScript : MonoBehaviour
+{
+    public BoulderRollingScript boulderRollingScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Detect player when they touch the pressure plate
+        if (other.tag == "Player")
+        {
+            boulderRollingScript.StartRolling(transform.position.x, transform.position.z);
+        }
+    }
+
+}
