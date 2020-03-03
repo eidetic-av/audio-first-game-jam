b8e38f701c83afd15a0777255bdb66f5e8af31fb
Luca <Lucas.venn@outlook.com>
2020-03-03 14:41:04 +1100

    Binds Pause to I
    Quickfix

--- Audio_First_Game_Jam/Assets/Scipts/Menu/PauseMenu.cs
+++ Audio_First_Game_Jam/Assets/Scipts/Menu/PauseMenu.cs
@@ -11,7 +11,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.I))
         {
             if (GameIsPaused)
             {
