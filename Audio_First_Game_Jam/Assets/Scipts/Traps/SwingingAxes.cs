6ed48a5478f548cf18cdc27ff41a103fe693e24e
Eric Dharmasiri <eric.dharmasiri@gmail.com>
2020-03-03 12:39:35 +1100

    Added Swinging Axes and Spikes

---
+++ Audio_First_Game_Jam/Assets/Scipts/Traps/SwingingAxes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingingAxes : MonoBehaviour
+{
+    private bool trapIsActive;
+    public GameObject axeToTrigger;
+    private Animator animationToTrigger;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationToTrigger = axeToTrigger.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(trapIsActive);
+        Debug.Log(animationToTrigger.GetBool("isTriggered"));
+        if (trapIsActive)
+        {
+            animationToTrigger.SetBool("isTriggered", true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            trapIsActive = true;
+        }
+    }
+}
